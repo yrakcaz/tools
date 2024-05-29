de589899b2e77bd31f4b3d59a2eb072f8a6f9df8
Zackary Ayoun <zackaryayoun@gmail.com>
2024-05-29 23:22:00 +0100

    update for nvim

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ git submodule update
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 cp -r dotfiles/.??* ~
+cp -r ~/.vim ~/.config/nvim
+cp -r ~/.vimrc ~/.config/nvim/init.vim
 
 cp -r scripts/* $PREFIX/bin
 
